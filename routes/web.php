fb317c56071f288d63a11ab86261df46d9e1284d
Paulo Vitor <pvitorv@gmail.com>
2026-02-09 22:45:35 -0300

    segurança

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ Route::get('/', function () {
 
 Route::get('/docs/criar-seu-portfolio', fn () => view('docs.criar-seu-portfolio'))->name('docs.criar-seu-portfolio');
 
-Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+Route::post('/contact', [ContactController::class, 'store'])->middleware('throttle:10,1')->name('contact.store');
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'login'])->middleware('throttle:5,1');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
